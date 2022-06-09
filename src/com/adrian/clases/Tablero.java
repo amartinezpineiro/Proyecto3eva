922a4a18b81938885ed3f045f4cceb293db14ab7
Adrian <amartinezpineiro@danielcastelao.org>
2022-06-09 15:23:10 +0200

    Monopoly v1.1

--- src/com/adrian/clases/Tablero.java
+++ src/com/adrian/clases/Tablero.java
@@ -142,5 +142,224 @@ public class Tablero {
         if (jugador.getPosicion() + avanzar > (listaCasillas.size() - 1)) {
             jugador.setPosicion(jugador.getPosicion() + avanzar - listaCasillas.size());
 
+        } else if (jugador.getPosicion() == (listaCasillas.size() - 1)) {
+            jugador.setPosicion(jugador.getPosicion() + avanzar - listaCasillas.size());
+
+        } else {
+            jugador.setPosicion(jugador.getPosicion() + avanzar);
+        }
+        //llamada al metodo que mueve las fichas
+        moverFichas(listaJugadores,jugador);
+
     }
-}
+    public void moverFichas(ArrayList<Jugador> listaJugadores, Jugador jugador){
+        if(jugador==listaJugadores.get(0)){
+            switch (jugador.getPosicion()){
+                case 0: mimarco.setJ1pos(790,754);
+                    break;
+                case 1: mimarco.setJ1pos(660,775);
+                    break;
+                case 2: mimarco.setJ1pos(560,775);
+                    break;
+                case 3: mimarco.setJ1pos(460,775);
+                    break;
+                case 4: mimarco.setJ1pos(360,775);
+                    break;
+                case 5: mimarco.setJ1pos(260,775);
+                    break;
+                case 6: mimarco.setJ1pos(110,754);
+                    break;
+                case 7: mimarco.setJ1pos(110,625);
+                    break;
+                case 8: mimarco.setJ1pos(110,525);
+                    break;
+                case 9: mimarco.setJ1pos(110,425);
+                    break;
+                case 10: mimarco.setJ1pos(110,325);
+                    break;
+                case 11: mimarco.setJ1pos(110,225);
+                    break;
+                case 12: mimarco.setJ1pos(110,80);
+                    break;
+                case 13: mimarco.setJ1pos(250,90);
+                    break;
+                case 14: mimarco.setJ1pos(350,90);
+                    break;
+                case 15: mimarco.setJ1pos(450,90);
+                    break;
+                case 16: mimarco.setJ1pos(550,90);
+                    break;
+                case 17: mimarco.setJ1pos(650,90);
+                    break;
+                case 18: mimarco.setJ1pos(770,80);
+                    break;
+                case 19: mimarco.setJ1pos(790,220);
+                    break;
+                case 20: mimarco.setJ1pos(800,320);
+                    break;
+                case 21: mimarco.setJ1pos(800,420);
+                    break;
+                case 22: mimarco.setJ1pos(800,520);
+                    break;
+                case 23: mimarco.setJ1pos(800,620);
+                    break;
+            }
+        }
+        if(jugador==listaJugadores.get(1)){
+            switch (jugador.getPosicion()){
+                case 0: mimarco.setJ2pos(790+60,754);
+                    break;
+                case 1: mimarco.setJ2pos(660+50,775);
+                    break;
+                case 2: mimarco.setJ2pos(560+50,775);
+                    break;
+                case 3: mimarco.setJ2pos(460+50,775);
+                    break;
+                case 4: mimarco.setJ2pos(360+50,775);
+                    break;
+                case 5: mimarco.setJ2pos(260+50,775);
+                    break;
+                case 6: mimarco.setJ2pos(110+60,754);
+                    break;
+                case 7: mimarco.setJ2pos(110+50,625);
+                    break;
+                case 8: mimarco.setJ2pos(110+50,525);
+                    break;
+                case 9: mimarco.setJ2pos(110+50,425);
+                    break;
+                case 10: mimarco.setJ2pos(110+50,325);
+                    break;
+                case 11: mimarco.setJ2pos(110+50,225);
+                    break;
+                case 12: mimarco.setJ2pos(110+60,80);
+                    break;
+                case 13: mimarco.setJ2pos(250+50,90);
+                    break;
+                case 14: mimarco.setJ2pos(350+50,90);
+                    break;
+                case 15: mimarco.setJ2pos(450+50,90);
+                    break;
+                case 16: mimarco.setJ2pos(550+50,90);
+                    break;
+                case 17: mimarco.setJ2pos(650+50,90);
+                    break;
+                case 18: mimarco.setJ2pos(770+50,80);
+                    break;
+                case 19: mimarco.setJ2pos(790+50,220);
+                    break;
+                case 20: mimarco.setJ2pos(800+50,320);
+                    break;
+                case 21: mimarco.setJ2pos(800+50,420);
+                    break;
+                case 22: mimarco.setJ2pos(800+50,520);
+                    break;
+                case 23: mimarco.setJ2pos(800+50,620);
+                    break;
+            }
+        }
+        if(jugador==listaJugadores.get(2)){
+            switch (jugador.getPosicion()){
+                case 0: mimarco.setJ3pos(790,754+50);
+                    break;
+                case 1: mimarco.setJ3pos(660,775+50);
+                    break;
+                case 2: mimarco.setJ3pos(560,775+50);
+                    break;
+                case 3: mimarco.setJ3pos(460,775+50);
+                    break;
+                case 4: mimarco.setJ3pos(360,775+50);
+                    break;
+                case 5: mimarco.setJ3pos(260,775+50);
+                    break;
+                case 6: mimarco.setJ3pos(110,754+50);
+                    break;
+                case 7: mimarco.setJ3pos(110,625+50);
+                    break;
+                case 8: mimarco.setJ3pos(110,525+50);
+                    break;
+                case 9: mimarco.setJ3pos(110,425+50);
+                    break;
+                case 10: mimarco.setJ3pos(110,325+50);
+                    break;
+                case 11: mimarco.setJ3pos(110,225+50);
+                    break;
+                case 12: mimarco.setJ3pos(110,80+50);
+                    break;
+                case 13: mimarco.setJ3pos(250,90+50);
+                    break;
+                case 14: mimarco.setJ3pos(350,90+50);
+                    break;
+                case 15: mimarco.setJ3pos(450,90+50);
+                    break;
+                case 16: mimarco.setJ3pos(550,90+50);
+                    break;
+                case 17: mimarco.setJ3pos(650,90+50);
+                    break;
+                case 18: mimarco.setJ3pos(770,80+50);
+                    break;
+                case 19: mimarco.setJ3pos(790,220+50);
+                    break;
+                case 20: mimarco.setJ3pos(800,320+50);
+                    break;
+                case 21: mimarco.setJ3pos(800,420+50);
+                    break;
+                case 22: mimarco.setJ3pos(800,520+50);
+                    break;
+                case 23: mimarco.setJ3pos(800,620+50);
+                    break;
+            }
+        }
+        if(jugador==listaJugadores.get(3)){
+            switch (jugador.getPosicion()){
+                case 0: mimarco.setJ4pos(790+60,754+50);
+                    break;
+                case 1: mimarco.setJ4pos(660+50,775+50);
+                    break;
+                case 2: mimarco.setJ4pos(560+50,775+50);
+                    break;
+                case 3: mimarco.setJ4pos(460+50,775+50);
+                    break;
+                case 4: mimarco.setJ4pos(360+50,775+50);
+                    break;
+                case 5: mimarco.setJ4pos(260+50,775+50);
+                    break;
+                case 6: mimarco.setJ4pos(110+60,754+50);
+                    break;
+                case 7: mimarco.setJ4pos(110+50,625+50);
+                    break;
+                case 8: mimarco.setJ4pos(110+50,525+50);
+                    break;
+                case 9: mimarco.setJ4pos(110+50,425+50);
+                    break;
+                case 10: mimarco.setJ4pos(110+50,325+50);
+                    break;
+                case 11: mimarco.setJ4pos(110+50,225+50);
+                    break;
+                case 12: mimarco.setJ4pos(110+60,80+50);
+                    break;
+                case 13: mimarco.setJ4pos(250+50,90+50);
+                    break;
+                case 14: mimarco.setJ4pos(350+50,90+50);
+                    break;
+                case 15: mimarco.setJ4pos(450+50,90+50);
+                    break;
+                case 16: mimarco.setJ4pos(550+50,90+50);
+                    break;
+                case 17: mimarco.setJ4pos(650+50,90+50);
+                    break;
+                case 18: mimarco.setJ4pos(770+50,80+50);
+                    break;
+                case 19: mimarco.setJ4pos(790+50,220+50);
+                    break;
+                case 20: mimarco.setJ4pos(800+50,320+50);
+                    break;
+                case 21: mimarco.setJ4pos(800+50,420+50);
+                    break;
+                case 22: mimarco.setJ4pos(800+50,520+50);
+                    break;
+                case 23: mimarco.setJ4pos(800+50,620+50);
+                    break;
+            }
+        }
+        }
+    }
